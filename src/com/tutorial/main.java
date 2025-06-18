1dc21b9b16272a1a368617cb8b28842d6b4d6e54
Fattan2005 <fattanhibrizi@gmail.com>
2025-06-18 14:38:19 +0700

    menampilkan data sederhana

--- src/com/tutorial/main.java
+++ src/com/tutorial/main.java
@@ -1,7 +1,27 @@
 package com.tutorial;
 
-public class main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+// membuat class sebagai template
+class Mahasigma {
+    String nama;
+    String umur;
+    String NPM;
+}
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        //membuat object
+        Mahasigma mahasigma = new Mahasigma();
+
+        //masukkan data
+        mahasigma.nama  = "Fattan Gamteng";
+        mahasigma.umur  = "20"
+        mahasigma.NPM   = "10123692";
+
+        //print out
+        System.out.println(mahasigma.nama);
+        System.out.println(mahasigma.umur);
+        System.out.println(mahasigma.NPM);
+
     }
 }
