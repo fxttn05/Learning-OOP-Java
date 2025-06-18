3dd269da6406532eae302ce2d2fdd289fece7d30
Fattan2005 <fattanhibrizi@gmail.com>
2025-06-18 14:43:06 +0700

    menambahkan data

--- src/com/tutorial/main.java
+++ src/com/tutorial/main.java
@@ -12,16 +12,25 @@ public class Main {
         
         //membuat object
         Mahasigma mahasigma = new Mahasigma();
+        Mahasigma mahasigma2 = new Mahasigma();
 
         //masukkan data
         mahasigma.nama  = "Fattan Gamteng";
-        mahasigma.umur  = "20"
+        mahasigma.umur  = "20";
         mahasigma.NPM   = "10123692";
+
+        mahasigma2.nama  = "Andi tidak gamteng";
+        mahasigma2.umur  = "22";
+        mahasigma2.NPM   = "10123999";
 
         //print out
         System.out.println(mahasigma.nama);
         System.out.println(mahasigma.umur);
         System.out.println(mahasigma.NPM);
+
+        System.out.println(mahasigma2.nama);
+        System.out.println(mahasigma2.umur);
+        System.out.println(mahasigma2.NPM);
 
     }
 }
